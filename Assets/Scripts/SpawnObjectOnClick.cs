4c5545744b364990f189c9160b888031f5c59a6a
Peter Stefek <peterlikesemail@gmail.com>
2018-06-26 16:32:13 -0700

    added unity project

---
+++ Assets/Scripts/SpawnObjectOnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnObjectOnClick : NetworkBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	public override void OnStartServer()
+	{
+        base.OnStartServer();
+
+	}
+
+    void Spawn() {
+        GameObject obj = Instantiate(target, new Vector3(0, 0, 0), Quaternion.identity);
+        NetworkServer.Spawn(obj);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Spawn();
+        }	
+	}
+}
