e3faba8cf6cc013db2cbf25879efa163480e2a92
Peter Stefek <peterlikesemail@gmail.com>
2018-06-26 17:19:49 -0700

    added sync need to tune parameters

--- Assets/Scripts/SpawnObjectOnClick.cs
+++ Assets/Scripts/SpawnObjectOnClick.cs
@@ -14,15 +14,26 @@ public class SpawnObjectOnClick : NetworkBehaviour {
 
 	}
 
-    void Spawn() {
-        GameObject obj = Instantiate(target, new Vector3(0, 0, 0), Quaternion.identity);
+    [Command]
+    void CmdSpawn(float x, float y) {
+        GameObject obj = Instantiate(target, new Vector3(x, y, 0), Quaternion.identity);
         NetworkServer.Spawn(obj);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isLocalPlayer)
+            return;
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane projectionPlane = new Plane(Vector3.forward, Vector3.zero);
+        float distance;
+        projectionPlane.Raycast(mouseRay, out distance);
+        Vector2 mousePos = mouseRay.GetPoint(distance);
+
         if (Input.GetMouseButtonDown(0)) {
-            Spawn();
+            CmdSpawn(mousePos.x, mousePos.y);
         }	
+        print(mousePos);
+        print(Input.mousePosition);
 	}
 }
