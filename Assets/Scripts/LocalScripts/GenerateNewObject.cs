c36bdf3af16831465d8453fa4977547bf40ea71f
psturmfels <psturm@umich.edu>
2018-06-26 20:07:21 -0700

    more hotfixes to indicator ciube

--- Assets/Scripts/LocalScripts/GenerateNewObject.cs
+++ Assets/Scripts/LocalScripts/GenerateNewObject.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class GenerateNewObject : MonoBehaviour {
+public class GenerateNewObject : NetworkBehaviour {
     public GameObject[] indicatorShapes;
     public GameObject[] generatedShapes;
 
     private CanSpawnLinkedObject canSpawn;
 
 	void Start () {
-        GameObject spawnedIndicator = Instantiate (indicatorShapes[0], transform.position, Quaternion.identity, transform);
-        canSpawn = GetComponent<CanSpawnLinkedObject>();
-        canSpawn.detector = spawnedIndicator.GetComponent<DetectInvalidPosition>();
+        if (isLocalPlayer)
+        {
+            GameObject spawnedIndicator = Instantiate(indicatorShapes[0], transform.position, Quaternion.identity, transform);
+            canSpawn = GetComponent<CanSpawnLinkedObject>();
+            canSpawn.detector = spawnedIndicator.GetComponent<DetectInvalidPosition>();
+        }
 	}
 }
